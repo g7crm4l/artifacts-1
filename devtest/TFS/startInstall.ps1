055c59e23bf6ffd8b2faad045af92d2000b7135a
yashg7cr <yashwin.yashwin3@gmail.com>
2017-05-31 17:38:34 +0530

    Update startInstall.ps1

--- devtest/TFS/startInstall.ps1
+++ devtest/TFS/startInstall.ps1
@@ -1,6 +1,7 @@
 $NewDIR = "C:\SoftwaresDump\QTP12.5"
 $SoftwareWebLink = "https://download.microsoft.com/download/a/e/1/ae184ba4-5926-4be6-a036-17b318960453/tfsserver2017.1.exe"
 $SoftwarePath = "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe"
+start-sleep -seconds "10"
 
 # Create temp directory
 New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
@@ -10,7 +11,6 @@ New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyCon
 Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe", "C:\SoftwaresDump\QTP12.5\tfssetup.exe"
 start-sleep -seconds "100"
 
-# Invoke Installion with powershell
-Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe", "C:\SoftwaresDump\QTP12.5\tfssetup.exe"
 
-Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe", "C:\SoftwaresDump\QTP12.5\tfssetup.exe"
+
+
