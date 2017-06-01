103f6e12f5bbaebc0559422c5b2795092b8456e2
yashg7cr <yashwin.yashwin3@gmail.com>
2017-06-01 14:52:05 +0530

    Update startInstall.ps1

--- devtest/TFS/startInstall.ps1
+++ devtest/TFS/startInstall.ps1
@@ -1,6 +1,25 @@
+$NewDIR = "C:\SoftwaresDump\QTP12.5"
+
+$SoftwareWebLink = "https://download.microsoft.com/download/a/e/1/ae184ba4-5926-4be6-a036-17b318960453/tfsserver2017.1.exe"
+
+
+$SoftwarePath = "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe"
+
+
+
 # Create temp directory
+
 New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
 
 # Download
-(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/a/e/1/ae184ba4-5926-4be6-a036-17b318960453/tfsserver2017.1.exe", "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe")
-Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe" /qn -Wait
+
+(New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
+
+(New-Object System.Net.WebClient).DownloadFile("http://artifacts.g7crm4l.org/softwares/QTP12.5/tfssetup.exe", "C:\SoftwaresDump\QTP12.5\tfssetup.exe")
+
+# Install
+
+Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe" -ArgumentList "/S /v""/qn""" -Wait  
+
+Start-Process "C:\SoftwaresDump\QTP12.5\tfssetup.exe" -ArgumentList "/S /v""/qn""" -Wait  
+
