941e1ce0f135926832b07174c8121fb98869fb1e
yashg7cr <yashwin.yashwin3@gmail.com>
2017-06-01 19:05:16 +0530

    qtpbat

--- devtest/demo/Start.bat
+++
@@ -1,2 +0,0 @@
-start "C:\SoftwaresDump\QTP12.5" setup.exe
-start "C:\SoftwaresDump\QTP12.5" qtpsetup.exe 
--- devtest/demo/startinstall.ps1
+++ devtest/demo/startinstall.ps1
@@ -1,6 +1,1 @@
-# Create temp directory
-New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
-
-# Download
-(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/a/e/1/ae184ba4-5926-4be6-a036-17b318960453/tfsserver2017.1.exe", "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe")
-Start-Process "C:\SoftwaresDump\QTP12.5\tfsserver2017.1.exe" /qn -Wait
+Start-Process "C:\SoftwaresDump\QTP12.5\start.bat"
